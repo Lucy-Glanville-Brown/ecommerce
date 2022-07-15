1edd2cb53b619656d2b58ff7b591dc5a83105f58
Lucy-Glanville-Brown <lucyglanvillebrown@gmail.com>
2022-07-15 22:14:34 +0000

    Adjusted reviews

--- reviews/views.py
+++ reviews/views.py
@@ -12,7 +12,7 @@ from profiles.models import UserProfile
 @login_required
 def add_review(request, product_id):
     """ Allows a user to add a review """
-
+    # John Routledge
     product = get_object_or_404(Product, pk=product_id)
     user = get_object_or_404(UserProfile, user=request.user)
 
@@ -24,7 +24,7 @@ def add_review(request, product_id):
             review.user = user
             review.save()
             messages.success(request, 'Thank You! Your review \
-                has now been posted!')
+                has been added!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
             messages.error(request, 'Oops, something went wrong! \
@@ -32,7 +32,31 @@ def add_review(request, product_id):
 
     context = {
         'form': form,
-        'product': product,
     }
 
-    return render(request, 'reviews/add_review.html', context)
+    return render(request, context)
+
+
+    # product = get_object_or_404(Product, pk=product_id)
+    # user = get_object_or_404(UserProfile, user=request.user)
+
+    # if request.user.is_authenticated:
+    #     form = ReviewForm(request.POST)
+    #     if form.is_valid():
+    #         review = form.save(commit=False)
+    #         review.product = product
+    #         review.user = user
+    #         review.save()
+    #         messages.success(request, 'Thank You! Your review \
+    #             has now been posted!')
+    #         return redirect(reverse('product_detail', args=[product.id]))
+    #     else:
+    #         messages.error(request, 'Oops, something went wrong! \
+    #             Please try adding your review again.')
+
+    # context = {
+    #     'form': form,
+    #     'product': product,
+    # }
+
+    # return render(request, 'reviews/add_review.html', context)
